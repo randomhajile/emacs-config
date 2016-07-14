252be29809c94578e36896effb4a5aba8955d0b1
Michael Hampton <mike.hampton@hireanesquire.com>
2016-07-14 17:44:20 -0400

    Dired omit files should be working more correctly now.

--- config/dired.el
+++ config/dired.el
@@ -1,20 +1,3 @@
-;; toggle showing dot files in dired
-(defun dired-dotfiles-toggle ()
-  "Show/hide dot-files"
-  (interactive)
-  (when (equal major-mode 'dired-mode)
-    (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
-        (progn
-          (set (make-local-variable 'dired-dotfiles-show-p) nil)
-          (message "h")
-          (dired-mark-files-regexp "^\\\.")
-          (dired-do-kill-lines))
-      (progn (revert-buffer) ; otherwise, just revert to re-show
-             (set (make-local-variable 'dired-dotfiles-show-p) t)))))
-
-(eval-after-load 'dired
-  '(define-key dired-mode-map (kbd "M-d") 'dired-dotfiles-toggle))
-
 ;; dired-x mode
 (add-hook 'dired-load-hook
           (function (lambda () (load "dired-x"))))
@@ -23,15 +6,28 @@
 (toggle-diredp-find-file-reuse-dir 1)
 
 ;; don't show me uninteresting files in dired
-(setq dired-omit-files 
-      (rx (or (seq bol (? ".") "#")         ;; emacs autosave files 
-              (seq "~" eol)                 ;; backup-files 
-              (seq bol "svn" eol)           ;; svn dirs 
+(setq dired-omit-files
+      (rx (or (seq bol (? ".") "#")         ;; emacs autosave files
+              (seq "~" eol)                 ;; backup-files
+              (seq bol "svn" eol)           ;; svn dirs
               (seq ".pyc" eol)
-              ))) 
-(setq dired-omit-extensions 
-      (append dired-latex-unclean-extensions 
-              dired-bibtex-unclean-extensions 
-              dired-texinfo-unclean-extensions)) 
-(add-hook 'dired-mode-hook (lambda () (dired-omit-mode 1))) 
-(put 'dired-find-alternate-file 'disabled nil)
+              )))
+
+;; omit dot files
+(setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
+
+(defun dired-dotfiles-toggle ()
+  "Show/hide dot-files"
+  (interactive)
+  (when (equal major-mode 'dired-mode)
+    (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+        (progn
+          (set (make-local-variable 'dired-dotfiles-show-p) nil)
+          (message "h")
+          (dired-mark-files-regexp "^.*?\\.")
+          (dired-do-kill-lines))
+      (progn (revert-buffer) ; otherwise, just revert to re-show
+             (set (make-local-variable 'dired-dotfiles-show-p) t)))))
+
+(eval-after-load 'dired
+  '(define-key dired-mode-map (kbd "M-d") 'dired-dotfiles-toggle))
