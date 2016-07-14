139925e238ec6351e412eccd56bf9ebf04d335a2
Michael Hampton <mike.hampton@hireanesquire.com>
2016-07-14 17:43:31 -0400

    Removed redundant def.

--- config/golang.el
+++ config/golang.el
@@ -12,9 +12,6 @@
 (defun auto-complete-for-go ()
   (auto-complete-mode 1))
 (add-hook 'go-mode-hook 'auto-complete-for-go)
-(defun auto-complete-for-go ()
-  (auto-complete-mode 1))
-(add-hook 'go-mode-hook 'auto-complete-for-go)
 (with-eval-after-load 'go-mode
   (require 'go-autocomplete))
 (defun go-mode-setup ()
