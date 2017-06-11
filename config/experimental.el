880c1ed89942f9c51148ea38d1ea25f1cee28771
Mike Hampton <michael.hampton1@teamaol.com>
2017-06-11 18:09:36 -0400

    Some tweaks to autocomplete. Note: I hate autocomplete.

--- config/experimental.el
+++ config/experimental.el
@@ -17,3 +17,25 @@
 (global-set-key (kbd "C-c c") 'pbcopy)
 (global-set-key (kbd "C-c v") 'pbpaste)
 (global-set-key (kbd "C-c x") 'pbcut)
+
+
+;; Company mode and fci mode don't play well together. Disable fci when there
+;; is an autocompletion popup.
+(defvar-local company-fci-mode-on-p nil)
+
+(defun company-turn-off-fci (&rest ignore)
+  (when (boundp 'fci-mode)
+    (setq company-fci-mode-on-p fci-mode)
+    (when fci-mode (fci-mode -1))))
+
+(defun company-maybe-turn-on-fci (&rest ignore)
+  (when company-fci-mode-on-p (fci-mode 1)))
+
+(add-hook 'company-completion-started-hook 'company-turn-off-fci)
+(add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+(add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+
+;; I hate autocomplete
+(when (require 'company nil t)
+  (remove-hook 'elpy-modules 'elpy-module-company)
+  (add-hook 'elpy-mode-hook 'company-mode))
