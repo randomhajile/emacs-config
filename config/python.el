7330bd12061e82075ba757d9cb09740684145dce
Michael Hampton <mhampton@moviepass.com>
2018-08-31 12:48:41 -0400

    Add flycheck to Python config.

--- config/python.el
+++ config/python.el
@@ -1,6 +1,12 @@
+(require 'flycheck)
 (require 'virtualenvwrapper)
 (elpy-enable)
 
 (setq venv-location "~/.virtualenvs")
 (setq elpy-rpc-backend "jedi")
 (venv-workon "emacs")
+(add-hook 'python-mode-hook 'flycheck-mode)
+
+(eval-after-load "elpy"
+  '(cl-dolist (key '("C-c C-n" "C-c C-p"))
+     (define-key elpy-mode-map (kbd key) nil)))
