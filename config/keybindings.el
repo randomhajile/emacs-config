188e8db66fc34b2551aad9745eeb8fdf8406f954
Michael Hampton <mhampton@moviepass.com>
2018-08-31 12:48:01 -0400

    Fix some whitespace issues and add flycheck keybindings to prog-mode.

--- config/keybindings.el
+++ config/keybindings.el
@@ -46,13 +46,19 @@ the region."
 (setq mouse-wheel-follow-mouth 1)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1]
-	     [triple-mouse-1] [mouse-2] [down-mouse-2] [drag-mouse-2]
-	     [double-mouse-2] [triple-mouse-2] [mouse-3] [down-mouse-3]
-	     [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+             [triple-mouse-1] [mouse-2] [down-mouse-2] [drag-mouse-2]
+             [double-mouse-2] [triple-mouse-2] [mouse-3] [down-mouse-3]
+             [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
              [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4]
-	     [triple-mouse-4] [mouse-5] [down-mouse-5] [drag-mouse-5]
-	     [double-mouse-5] [triple-mouse-5]))
+             [triple-mouse-4] [mouse-5] [down-mouse-5] [drag-mouse-5]
+             [double-mouse-5] [triple-mouse-5]))
   (global-unset-key k))
 
 ;; avy
 (global-set-key (kbd "M-j") 'avy-goto-word-or-subword-1)
+
+;; flycheck for prog-mode
+(add-hook 'prog-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-n") #'flycheck-next-error)))
+(add-hook 'prog-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-p") #'flycheck-previous-error)))
