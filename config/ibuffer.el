5146d1245cecc50771abf0d652bbc1989d0663c9
Mike Hampton <mhampton@codecademy.com>
2020-12-29 12:31:16 -0500

    Clean up ibuffer modes.

--- config/ibuffer.el
+++ config/ibuffer.el
@@ -7,18 +7,11 @@
                          (name . "^\\*Messages\\*$")))
                ("git" (name . "^\\magit[-:]"))
                ("golang" (mode . go-mode))
-               ("haskell" (mode . haskell-mode))
                ("html" (mode . html-mode))
-               ("lisp" (or
-                        (mode . emacs-lisp-mode)
-                        (mode . lisp-mode)
-                        (mode . clojure-mode)))
                ("org" (mode . org-mode))
+               ("ruby" (mode . ruby-mode))
                ("python" (mode . python-mode))
-               ("ruby" (or
-                        (mode . ruby-mode)
-                        (mode . enh-ruby-mode)))))))
-
+               ("terraform" (mode . terraform-mode))))))
 (add-hook 'ibuffer-mode-hook
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "default")))
