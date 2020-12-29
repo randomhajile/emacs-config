26f2283fd307101aa001661d3f117c351713816f
Mike Hampton <mhampton@codecademy.com>
2020-12-29 12:31:41 -0500

    Update package management init.

--- config/package-management.el
+++ config/package-management.el
@@ -1,22 +1,10 @@
 (require 'package)
 
-;; Make sure el-get is available.
-;; (unless (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-;;     (goto-char (point-max))
-;;     (eval-print-last-sexp)))
-
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 
-
-;; (el-get 'sync)
 (package-initialize)
