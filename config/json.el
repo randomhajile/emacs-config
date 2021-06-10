c05c7edf14929897cb4c53cc5cb6793983cd1789
Michael Hampton <mhampton@hey.com>
2021-06-10 11:54:09 -0400

    Simplify json beautify hook.

--- config/json.el
+++ config/json.el
@@ -1,11 +1,4 @@
-(defun json-save-hook ()
-  "Applies json-mode-beautify twice to not disorient the user."
-  (interactive)
-  (json-mode-beautify)
-  (json-mode-beautify))
-
-;; json-mode-beautify twice before save
 (add-hook 'json-mode-hook
-	  (lambda ()
-	    (add-hook
-	     'before-save-hook 'json-save-hook t 'make-it-local)))
+          (lambda ()
+            (add-hook
+             'before-save-hook 'json-pretty-print-buffer t 'make-it-local)))
