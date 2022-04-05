99fcfe24f5fd67f0dd9b0718609dde7996174e30
Michael Hampton <mhampton@hey.com>
2022-04-05 11:53:41 -0400

    Update ibuffer for Typescript.

--- config/ibuffer.el
+++ config/ibuffer.el
@@ -13,7 +13,12 @@
                ("ruby" (mode . ruby-mode))
                ("shell" (mode . sh-mode))
                ("yaml" (mode . yaml-mode))
-               ("terraform" (mode . terraform-mode))))))
+               ("terraform" (mode . terraform-mode))
+               ("tide-server" (name . "^\\*tide-server\\*"))
+               ("typescript" (mode . typescript-mode))))))
+
 (add-hook 'ibuffer-mode-hook
           (lambda ()
-            (ibuffer-switch-to-saved-filter-groups "default")))
+            (ibuffer-switch-to-saved-filter-groups "default")
+            (setq ibuffer-hidden-filter-groups (list "tide-server"))
+            (ibuffer-update nil t)))
